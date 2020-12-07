c748602cbdf31c0224fab3623bfa7bdd6b3e0d67
blyal <blyal@protonmail.com>
2020-12-07 19:22:41 +0100

    A012: BEE podcast blurb
    Wrote and added proper blurb to BEE podcast component

--- src/components/coolstuff/podcasts/Ellis.js
+++ src/components/coolstuff/podcasts/Ellis.js
@@ -15,15 +15,11 @@ function Ellis() {
                     </a>
                 </div>
                 <div className="pocket-blurb">
-                    <p>Tim Ferriss is an entrepreneur, author, angel investor, and podcaster who has achieved massive success in all of these fields.
-                        In his podcast, it's his job, in his own words, to "deconstruct world-class performers from eclectic areas to extract the tactics, tools, and routines you can use. This includes favorite books, morning routines, exercise habits, time-management tricks, and much more."
-                        Ferriss has interviewed everyone, from actors such as Arnold Schwarzenegger, to CEOs such as Disney's Bob Iger, to psychotherapists such as relationship coach Esther Perel.
+                    <p>
+                        This one is for the film nerds, or should I say, the film evangelicals, as well as the literary folk (as long as you’re not a “snowflake”). Bret Easton Ellis is the bestselling author and screenwriter of great works such as “American Psycho” and “Less Than Zero”, and in more recent years has evolved into a director and podcaster. Ellis has a fearsome knowledge of film history, and I have to give this podcast a lot of credit for exponentially expanding my knowledge of that, and of the entertainment industry more generally, as Ellis’s interviews with important and notable cultural figures really opened my eyes to the fact that films don’t come out of a vacuum. They are specific to a time and a place and a context, they are part of a tradition, and they are made by people, just like you and me, who work together in order to both express something and to make some money (though admittedly sometimes only the latter).
                     </p>
                     <p>
-                        I've been a listener of this podcast for over five years now, and it has had a huge impact on my life. I was lucky to be introduced to it at a particularly formative time in my life, so was able to incorporate and adopt many of the tools and routines that Ferriss uncovers from his guests.
-                        Ferriss is something of a self–experimenter, having personally tried many of these tools and routines himself.
-                        He is also an promoter of human wellbeing, and his podcast and writings could be seen as a gift to anyone who reaches out to take it.
-                        His fascination with being able to maximise your chances of success, in all different domains, has allowed many people the chance to change their life outcomes by intelligently experimenting with their life recipes.
+                    I would say though that more important than teaching me that, Ellis has been a role model for me growing up as a young adult interested in self–expression. I don’t think I’ve ever come across someone so unafraid to express their genuine thoughts and feelings, in spite of whatever the mob narrative happens to be. It’s very easy to just go along with popular opinion, and I sometimes catch myself doing it even if it’s something I don’t intend to do. Ellis was the first person who really made me aware of this, and has been a good example of staying true to yourself, even if it means drawing the displeasure of others. On top of that Ellis is a great thinker and speaker, he articulates his ideas very well in conversation with the interesting guests he invites on his podcast, and he is a fantastic creator: every podcast is an impressive work in of itself.
                     </p>
                 </div>
                 <div className="recommended-things ellis-recommendations">
