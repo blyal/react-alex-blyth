f97a69de4a121ef649936cdaa673e985d9134ea9
blyal <blyal@protonmail.com>
2020-12-10 11:47:13 +0100

    XTRA: delete commented-out old icons in PicLinks
    Old icons that were pictures. They have been replaced by fontawesome

--- src/components/layout/PicLinks.js
+++ src/components/layout/PicLinks.js
@@ -14,9 +14,4 @@ const PicLinks = (props) => {
     )
 }
 
-// Old icons
-{/* <li><a target="_blank" rel="noopener noreferrer" href="mailto:alexblyth@pm.me"><img src={'/images/email-icon.png'} alt="Email alexblyth@pm.me" id="email-icon" className="icon" height="33.75" width="45" /></a></li>
-<li><a target="_blank" rel="noopener noreferrer" href="http://instagram.com/_alexblyth"><img src={'/images/instagram-icon.png'} alt="Instagram @alexblyth97" id="insta-icon" className="icon" height="60" width="80" /></a></li>
-<li><a target="_blank" rel="noopener noreferrer" href="http://github.com/blyal"><img src={'/images/github-icon.png'} alt="github icon" id="github-icon" className="icon" height="64" width="64" /></a></li> */}
-
 export default PicLinks;
