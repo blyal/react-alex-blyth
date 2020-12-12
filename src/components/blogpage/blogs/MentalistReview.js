22b44c30a0da1c0eb0eddf06d39eb6ec9deb5bba
blyal <blyal@protonmail.com>
2020-12-12 17:36:49 +0100

    A032: add some captions to mentalist photos
    Added Figure parent to images in mentalist blog post. Added figcaption
    child underneath img, with captions

--- src/components/blogpage/blogs/MentalistReview.js
+++ src/components/blogpage/blogs/MentalistReview.js
@@ -11,13 +11,19 @@ class MentalistReview extends React.Component {
                 There are lots of good TV shows being made at the moment, far more worth watching than <i>The Mentalist</i>. However, sometimes something strange and slightly annoying happens to me, where I start to feel nostalgic for the bad TV shows that I used to binge as a teenager. Okay maybe it’s a little mean to call <i>The Mentalist</i> bad… it's good enough for me to see some cool stuff that the creators did with the show and to blog about it.
                 </p>
                 <div className="code-blog-entry-content-img">
-                    <img src={'images/mentalist-jane.jpg'} alt="Patrick Jane from The Mentalist" height='100%' width='100%'/>
+                    <figure>
+                        <img src={'images/mentalist-jane.jpg'} alt="Patrick Jane from The Mentalist" height='100%' width='100%'/>
+                        <figcaption>Jane's dashing smile</figcaption>
+                    </figure>
                 </div>
                 <p>
                 So what is good about the show? Firstly, the characters and the acting of the main cast is superb. Simon Baker plays Patrick Jane, a conman ‘psychic’ who decides to use his abilities for good after his family is killed by a serial killer whom he publicly offended while appearing on a talk show. Jane then joins a team of homicide police as a consultant, helping them solve murder cases while he pursues his ultimate goal of finding and getting revenge on his family’s killer. Simon Baker is an absolute star. He really holds your attention well, his delivery and demeanour are excellent, and he really brings the character to life. Pay attention for the very rare but definite moments where his Australian accent slips through. Also would give a shout out to the actors of Teresa Lisbon (Jane’s boss) and Kimball Cho. Robin Tunney plays one of the cutest homicide detectives I’ve ever seen, is very emotionally engaging, and Tim Kang’s Cho is an extremely funny deadpan and badass performance. All three are very likeable, which is why the show lasted for so long.
                 </p>
                 <div className="code-blog-entry-content-img">
-                    <img src={'images/mentalist-lisbon.jpg'} alt="Teresa Lisbon from The Mentalist" height='100%' width='100%'/>
+                    <figure>
+                        <img src={'images/mentalist-lisbon.jpg'} alt="Teresa Lisbon from The Mentalist" height='100%' width='100%'/>
+                        <figcaption>Cute as a button Teresa Lisbon, Jane's parter–in–crime</figcaption>
+                    </figure>
                 </div>
                 <p>
                 The writing of the show is a little generic and predictable. The general formula of each episode is that the team starts at a fresh crime scene, Jane insults some local authority figure and has some banter with his team, and they solve the case simultaneously to one member of the team working through some personal issue or circumstance. There is usually some bureaucratic matter at play in the background of their division, the California Bureau of Investigation (which is actually one of the show’s strengths, as it shows how the politics of law enforcement and government agencies can affect the course-of-duty). The show introduces a series of possible candidates for the murder, and Jane always inconceivably manages to unmask the perpetrator, who is usually the not–obvious choice. Some episodes vary from the formula, and more frequently as the show progresses, but the formula starts to get tedious by about the fourth season.
@@ -26,13 +32,19 @@ class MentalistReview extends React.Component {
                 I have to say, though, that the show has some good seasons, and some episodes are actually great. The best episodes were the ones that dealt with the ongoing ‘Red John’ storyline (chasing the notorious and elusive serial killer that killed Jane’s family). As smart as Jane is, and as well as he is able to read people and situations, Red John is always one step ahead, and Jane is always looking for creative ways to gain the upper hand. There were about two or three ‘Red John’ episodes every season, until the fifth and sixth seasons where the intensity heightens as Jane closes in on Red John’s true identity (so luckily the formula goes on hiatus for a few episodes between seasons 5 and 6).
                 </p>
                 <div className="code-blog-entry-content-img">
-                    <img src={'images/mentalist-redjohn.jpg'} alt="The Red John symbol from The Mentalist" height='100%' width='100%'/>
+                    <figure>
+                        <img src={'images/mentalist-redjohn.jpg'} alt="The Red John symbol from The Mentalist" height='100%' width='100%'/>
+                        <figcaption>The haunting face drawn by Red John in his victims' blood. <br />Jane symbolically lets the one drawn in his house stay up there.</figcaption>
+                    </figure>
                 </div>
                 <p>
                 The most interesting element to the show for me is the psychoanalytical aspect to it. Jane used to pretend to be a psychic, but basically what he is is a psychoanalyst for hire. He has an extraordinary understanding of human psychology, understands that the patterns of the past play out in the present in human behaviour, and uses that understanding to profile people associated with a murder investigation, as well as to play with and occasionally manipulate the people around him at work. Co-workers at the CBI often take offence to Jane’s arrogance (apart from those in his team, who love him), but anyone who tries to undermine him or diminish him ends up playing the price, as Jane is far too clever at setting traps for people, or else giving them the rope to hang themselves with (not literally). For all his manipulation, though, Jane is a good guy — the only person he wishes genuine harm towards, except for the occasional psychopath he might encounter in his line of work, is Red John. And that is the most intriguing thing: Jane is able to psychoanalyse people extremely effectively, but he has a very limited ability to address his own inner turmoil. He cannot let go of the guilt he feels for the death of his family, and rather than addressing the emotions and pain — rather than working through and letting go of his pain — he chases and wishes to kill Red John as an attempt to eliminate the pain, therefore living inside his trauma every day. It guides his reality. On the one hand, that’s good because he is ridding the world of a vicious killer. On the other hand, it’s terrible from the perspective of personal mental wellbeing. Killing Red John is not going to bring his family back.
                 </p>
                 <div className="code-blog-entry-content-img">
-                    <img src={'images/mentalist-cho.jpg'} alt="Cho from The Mentalist" height='100%' width='100%'/>
+                    <figure>
+                        <img src={'images/mentalist-cho.jpg'} alt="Cho from The Mentalist" height='100%' width='100%'/>
+                        <figcaption>Jane and Cho having a moment</figcaption>
+                    </figure>
                 </div>
                 <p>
                 The show is not phenomenal, but there are some really great moments. The episodes that lead up to Jane eventually meeting Red John (the real Red John) are fantastic, and the revelation of Red John’s identity is very very well–executed. The following seasons which deal with Jane and Lisbon’s relationship are vaguely satisfying, and their relationship is well–written, but the episodes are mostly bad — the police work is almost just an excuse to deal with the relationship, and it seems that little effort was put into the writing of the episodes. Seasons 1–5, before the post–RJ attempt to reinvent the show, occasionally had brilliant episodes. I will never forget the chills I got during ‘The Red Barn’, from Season 5 — not once, but several times during the episode. <i>The Mentalist</i> is for you if you like cop shows, showmanship, good acting and cast, maybe have a little interest in psychology, and don’t mind some repetitiveness. I’m really selling this show right?
