ae227b2ee204fd0e54059dde216e9035787ffbf8
blyal <blyal@protonmail.com>
2020-12-18 18:21:42 +0100

    Updates

---
+++ precache-manifest.66e4e6f5ca0e7bee74f7e27a072209e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b532b3fa5c55160ba251e37f8b3c3654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2035a8b0426199aca67",
+    "url": "/static/css/main.a9310425.chunk.css"
+  },
+  {
+    "revision": "f58419da294484324938",
+    "url": "/static/js/2.93a57ad2.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/static/js/2.93a57ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2035a8b0426199aca67",
+    "url": "/static/js/main.fb20dd8e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "6c800b00b7dc8c8e238512df5f206ec8",
+    "url": "/static/media/nice-sign.6c800b00.png"
+  },
+  {
+    "revision": "a40eeeec7b155f8ceb24765157373831",
+    "url": "/static/media/pink-brush.a40eeeec.png"
+  },
+  {
+    "revision": "315a6d793a5e92ca2de95bae3053487c",
+    "url": "/static/media/smoke.315a6d79.png"
+  }
+]);
