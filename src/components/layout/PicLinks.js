974b0bea1778915bb8f7e7974728af799a31e146
blyal <blyal@protonmail.com>
2021-01-26 21:34:17 +0100

    A074: add LinkedIn icon
    added fontawesome linkedin icon to piclinks component
    also changed order of the pic links

--- src/components/layout/PicLinks.js
+++ src/components/layout/PicLinks.js
@@ -3,13 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const PicLinks = (props) => {
     return (
         <ul className="thing-1">
-            <li><a target="_blank" rel="noopener noreferrer" href="mailto:alexblyth@pm.me"><FontAwesomeIcon icon={faEnvelope} size="2x" className="email-icon" /></a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="http://instagram.com/_alexblyth"><FontAwesomeIcon icon={faInstagram} size="2x" className="insta-icon" /></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alex-blyth-575107170/"><FontAwesomeIcon icon={faLinkedin} size="2x" className="linkedin-icon" /></a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="http://github.com/blyal"><FontAwesomeIcon icon={faGithub} size="2x" className="github-icon" /></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="mailto:alexblyth@pm.me"><FontAwesomeIcon icon={faEnvelope} size="2x" className="email-icon" /></a></li>
         </ul>
     )
 }
