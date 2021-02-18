18d554ebe87ad03cb00091ec21504dcca526dc85
blyal <blyal@protonmail.com>
2021-02-18 13:54:33 +0100

    XTRA: Privacy policy content changed (blurb)

--- src/components/additional/PrivacyPolicy.js
+++ src/components/additional/PrivacyPolicy.js
@@ -8,7 +8,7 @@ const PrivacyPolicy = (props) => {
             <div className='privacy-blurb'>
                 <p>This website is totally noncommercial and no data is shared with anyone.</p>
                 <p>But — the technology used to make this website may leave cookies on your device.</p>
-                <p>These are not harmful or commercial cookies — I avoid using untrustworthy technologies. I do use Google,
+                <p>These are not harmful or commercial cookies — I avoid using untrustworthy technologies. Google's services are used,
                    but only for the ReCaptcha Security plugin on the subscribe page, otherwise avoiding them is a high priority. DuckDuckGo for life!
                 </p>
                 <p>By pressing the button below, you give consent for cookies (again, the bare minimum) to be used on your device.</p>
