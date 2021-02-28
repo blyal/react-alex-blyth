ee731160d5eadd29f59bb7fd79d887d93f1adb1c
blyal <blyal@protonmail.com>
2021-02-28 16:20:16 +0100

    A083: create new blog post
    Added new blog about Interviewing
    New component added containing blog content
    photos for blog in new enclosing folder
    some CSS additions
    Added to the state of the BlogPage
    'keywords' key added to blogpage state blogentries (for later usage
    in upcoming commit)

--- src/components/blogpage/BlogPage.js
+++ src/components/blogpage/BlogPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Blog from './Blog';
 import MentalistReview from './blogs/MentalistReview';
 import HowToEmployment from './blogs/HowToEmployment';
+import HowToInterview from './blogs/HowToInterview';
 import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
 
@@ -10,14 +11,25 @@ class BlogPage extends React.Component {
         blogEntries: [
             {
                 id: "top-blog",
+                keyword: 'interviews',
+                title: 'How to Get Good at Job Interviews',
+                date: '19th February 2021',
+                open: true,
+                content: <HowToInterview />,
+                img: <img src={'images/blog-interviews/views.jpg'} alt="Mountain Body" height='95%' width='95%' className='code-blog-img'/>
+            },
+            {
+                id: uuid(),
+                keyword: 'jobsearching',
                 title: 'How to Get a Job during a Pandemic',
                 date: '13th January 2021',
-                open: true,
+                open: false,
                 content: <HowToEmployment />,
                 img: <img src={'images/Alex-Empadronado-Spain.jpg'} alt="Getting a Job in Spain" height='95%' width='95%' className='code-blog-img'/>
             },
             {
                 id: uuid(),
+                keyword: 'mentalist',
                 //apparently the italics tag below needs a key, because it's part of a list. Doesn't make any difference
                 title: ['Review of ', <i key="mentalist-italics">The Mentalist</i>, ' (2008 – 2015)'],
                 date: '21st October 2020',
